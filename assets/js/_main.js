395d50a59dff2a69576590bbb0678e5aabf045ab
Artem Gurzhii <artem.gurzhii@gmail.com>
2017-07-10 17:32:30 +0300

    adding comments and wrapping code into iife for the variables scoping

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,12 +1,20 @@
-const _anchors = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+// Adding IIFE for variables scoping
+(() => {
+  const _anchors = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
 
-// adds anchor permalinks to any Header tag that has an id
-[..._anchors].forEach(anchor => {
-  if (anchor.id) {
-    const link = document.createElement('a');
-    link.dataset.icon = '';
-    link.classList.add('anchor-link');
-    link.href = `#${anchor.id}`;
-    anchor.insertBefore(link, anchor.firstChild);
-  }
-});
+  // Add anchor permalinks to any Header tag that has an id
+  // Iterate over each header in array
+  [..._anchors].forEach(anchor => {
+
+    // If this header has an 'ID' attrbiute
+    if (anchor.id) {
+
+      // Create new link element, add attrbiutes and append it to the beggining or the header.
+      const link = document.createElement('a');
+      link.dataset.icon = '';
+      link.classList.add('anchor-link');
+      link.href = `#${anchor.id}`;
+      anchor.insertBefore(link, anchor.firstChild);
+    }
+  });
+})();
