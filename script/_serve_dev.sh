a13d5e755b7fe60ca35d407593fc4ad423e5df30
Michael Lynch <mtlynch@users.noreply.github.com>
2017-07-17 10:11:52 -0400

    Revert "Travis deploy task (#77)"
    This reverts commit 52784f2ccdffc4ad28d828b73526a7a4345c370f.

--- script/_serve_dev.sh
+++ script/_serve_dev.sh
@@ -1,10 +1,1 @@
-#!/usr/bin/env bash
-
-# terminate the script if any command fails
-set -e
-
-# Run JS build task
-npm run build:js
-
-# Start jekyll server
 bundle exec jekyll serve --config _config.yml,_config_dev.yml --watch
