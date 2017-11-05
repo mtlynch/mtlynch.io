8a05e12800e4cf6cb8d020f669ac3af701cd0166
Michael Lynch <michael@mtlynch.io>
2017-11-04 23:20:15 -0400

    Updating

--- val.cpp
+++ val.cpp
@@ -50,9 +50,5 @@
 
 
 bool Document::ValidateAndSerialize(Handle* output_handle) {
-  ValidationResult validation_result = validator_->Validate(&contents_);
-  if (validation_result == ValidationResult::Failure) {
-    return false;
-  }
   return serializer_->Serialize(&contents_);
 }
