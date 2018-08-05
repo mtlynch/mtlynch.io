1fa0329527d13455c160f3c9a347773b6003c05e
Andrew Newhouse <andrewwn@gmail.com>
2018-08-05 16:55:22 -0400

    updating yml tests to run as command

---
+++ _plugins/lib/jekyll-frontmatter-test.rb
@@ -0,0 +1,7 @@
+module Jekyll
+  module FrontmatterTest
+    autoload :VERSION, 'jekyll/frontmatter-test/version.rb'
+  end
+end
+
+require 'jekyll/commands/frontmatter-test.rb'
---
+++ _plugins/lib/jekyll/frontmatter-test/version.rb
@@ -0,0 +1,5 @@
+module Jekyll
+  module FrontmatterTest
+    VERSION = '0.0.1'
+  end
+end
