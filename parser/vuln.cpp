3fe52d8f79c33a3610d5526ba3a97756d749e085
Michael Lynch <git@mtlynch.io>
2020-11-27 17:04:56 -0500

    Add vulnerable cpp

---
+++ parser/vuln.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string.h>
+
+class Hat {
+
+};
+
+class HatManager {
+public:
+	HatManager();
+	void Rename();
+private:
+	Hat* hatCollection;
+	size_t hatCollectionSize;
+	Hat* reserveHats;
+	size_t reserveHatsSize;
+	char* name;
+	size_t nameLen;
+	char* newName;
+	size_t newNameLen;
+};
+
+
+HatManager::HatManager() {}
+
+void HatManager::Rename() {
+    strncpy(newName, name, newNameLen);
+    delete[] newName;
+}
+
+int main()
+{
+  std::cout << "What is your name? ";
+}
