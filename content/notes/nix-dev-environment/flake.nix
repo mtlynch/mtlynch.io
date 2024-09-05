61e659f0367b548a5a5688a6c52c86d2af01667d
Michael Lynch <mtlynch@users.noreply.github.com>
2024-09-04 20:23:26 -0400

    Format nix flakes (#1258)

--- content/notes/nix-dev-environment/flake.nix
+++ content/notes/nix-dev-environment/flake.nix
@@ -8,12 +8,14 @@
     python_dep.url = "github:NixOS/nixpkgs/517501bcf14ae6ec47efd6a17dda0ca8e6d866f9";
   };
 
-  outputs = { self, flake-utils, python_dep }@inputs :
-    flake-utils.lib.eachDefaultSystem (system:
-    let
+  outputs = {
+    self,
+    flake-utils,
+    python_dep,
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system: let
       python_dep = inputs.python_dep.legacyPackages.${system};
-    in
-    {
+    in {
       devShells.default = python_dep.mkShell {
         packages = [
           python_dep.python2
